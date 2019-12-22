f4f7e5b2771a134b4950fc23d472aee1ec1626e5
Christine Trant <christinetrant@gmail.com>
2019-12-22 15:22:20 +0000

    fixed spelling error in index.html, UIController now gets inout values and returns them through getInput function which can be called from AppController

--- app.js
+++ app.js
@@ -11,7 +11,17 @@ let budgetController = (function() {
 // UI Controller Module - IIFE
 let UIController = (function() {
 
-	//code in here
+	// Public method so needs to be returned in object
+	return {
+		getInput: function() {
+			return {
+				type: document.querySelector('.add__type').value, //Will be either inc or exp as it is a select dropdown
+				description: document.querySelector('.add__description').value,
+				value: document.querySelector('.add__value').value,
+			}
+		}
+	}
+
 
 })();
 
@@ -24,9 +34,11 @@ let controller = (function(budgetCtrl, UICtrl) {
 
 	const controlAddItem = function() {
 		// To do list when item is added:
-		
+
 		// 1. Get the field input data
-		
+		let input = UICtrl.getInput();
+		console.log(input);
+
 		// 2. Add the item to the budget controller
 		
 		// 3. Add the new item to the User Interface
@@ -40,13 +52,12 @@ let controller = (function(budgetCtrl, UICtrl) {
 	
 	// set up event listeners for add button or when enter is pressed.
 	document.querySelector('.add__btn').addEventListener('click', controlAddItem);
-
 	document.addEventListener('keypress', function(event) {
 		// Number 13 is the "Enter" key on the keyboard
 		if (event.keyCode === 13 || event.which === 13) {
-			// console.log('Enter was pressed');
 			controlAddItem();
 		}
+
 	});
 
 })(budgetController, UIController);
