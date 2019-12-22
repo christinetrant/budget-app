cde828889a9b0a07ac50b64f8fe9098a154fe288
Christine Trant <christinetrant@gmail.com>
2019-12-22 15:03:46 +0000

    added first event listener for when item is added by pressing enter or clicking on button

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 // Budget Controller Module - IIFE 
 let budgetController = (function() {
 
-	let x = 23;
+	// some code
 
-	let add = function(a) {
-		return x+a;
-	}
-
-	// This returns the method below which can be accessed outside of budgetController - the method below can always access the methods above due to closures.  
-	return {
-		publicTest: function(b) {
-			return add(b);
-		}
-	}
 })();
+
+
+
 
 // UI Controller Module - IIFE
 let UIController = (function() {
@@ -22,16 +15,38 @@ let UIController = (function() {
 
 })();
 
+
+
+
 // App Controller Module - IIFE
 // Call 2 parameters in the function for the above 2 functions
 let controller = (function(budgetCtrl, UICtrl) {
-	
-	let z = budgetCtrl.publicTest(5);
 
-	return {
-		anotherPublic: function() {
-			console.log(z);
-		}
+	const controlAddItem = function() {
+		// To do list when item is added:
+		
+		// 1. Get the field input data
+		
+		// 2. Add the item to the budget controller
+		
+		// 3. Add the new item to the User Interface
+		
+		// 4. Calculate the budget
+		
+		// 5. Display the budget on the UI
+
+		console.log('It works!');
 	}
+	
+	// set up event listeners for add button or when enter is pressed.
+	document.querySelector('.add__btn').addEventListener('click', controlAddItem);
+
+	document.addEventListener('keypress', function(event) {
+		// Number 13 is the "Enter" key on the keyboard
+		if (event.keyCode === 13 || event.which === 13) {
+			// console.log('Enter was pressed');
+			controlAddItem();
+		}
+	});
 
 })(budgetController, UIController);
