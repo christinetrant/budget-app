4edcb4fc32cd8b5fea6396f5dde3620c1f13af14
Christine Trant <christinetrant@gmail.com>
2019-12-22 15:31:13 +0000

    tidying up and putting all strings to be called from DOM in private function but can be called upon

--- app.js
+++ app.js
@@ -10,15 +10,25 @@ let budgetController = (function() {
 
 // UI Controller Module - IIFE
 let UIController = (function() {
-
+	//private variable
+	let DOMstrings = {
+		inputType: '.add__type',
+		inputDescription: '.add__description',
+		inputValue: '.add__value',
+		inputBtn: '.add__btn',
+	}
 	// Public method so needs to be returned in object
 	return {
 		getInput: function() {
 			return {
-				type: document.querySelector('.add__type').value, //Will be either inc or exp as it is a select dropdown
-				description: document.querySelector('.add__description').value,
-				value: document.querySelector('.add__value').value,
+				type: document.querySelector(DOMstrings.inputType).value, //Will be either inc or exp as it is a select dropdown
+				description: document.querySelector(DOMstrings.inputDescription).value,
+				value: document.querySelector(DOMstrings.inputValue).value,
 			}
+		},
+		// returns DOMstrings so they can be accessed publicly
+		getDomstrings: function() {
+			return DOMstrings;
 		}
 	}
 
@@ -31,6 +41,8 @@ let UIController = (function() {
 // App Controller Module - IIFE
 // Call 2 parameters in the function for the above 2 functions
 let controller = (function(budgetCtrl, UICtrl) {
+	//we need to access DOMstrings from UI Controller
+	const DOM = UICtrl.getDomstrings();
 
 	const controlAddItem = function() {
 		// To do list when item is added:
@@ -51,7 +63,7 @@ let controller = (function(budgetCtrl, UICtrl) {
 	}
 	
 	// set up event listeners for add button or when enter is pressed.
-	document.querySelector('.add__btn').addEventListener('click', controlAddItem);
+	document.querySelector(DOM.inputBtn).addEventListener('click', controlAddItem);
 	document.addEventListener('keypress', function(event) {
 		// Number 13 is the "Enter" key on the keyboard
 		if (event.keyCode === 13 || event.which === 13) {
