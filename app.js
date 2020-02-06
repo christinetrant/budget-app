eb825b39e3f75ba5a8998a29010b05f464de5e58
Christine Trant <christinetrant@gmail.com>
2020-02-06 21:26:55 +0000

    Updating budget controller to calculate budget totals and percentage

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ let budgetController = (function() {
 		this.value = value;
 	}
 
+	// all items below will either be of type inc or exp
+	let calculateTotal = function(type) {
+		var sum = 0;
+		data.allItems[type].forEach(function(current) {
+			sum += current.value;
+		});
+		data.totals[type] = sum;
+	}
+
 	// let allExpenses = [];
 	// let allIncomes = [];
 	// let totalIncome = 0;
@@ -54,6 +63,33 @@ let budgetController = (function() {
 			data.allItems[type].push(newItem);
 			//return the new element
 			return newItem;
+		},
+
+		//Calculate total income & expenses
+		calculateBudget: function() {
+			calculateTotal('exp');
+			calculateTotal('inc');
+
+			//calculate the budget: income - expenses
+			data.budget = data.totals.inc - data.totals.exp;
+
+			//Calculate the percentage of income that we spent
+			if(data.totals.inc > 0) {
+				data.percentage = Math.round((data.totals.exp / data.totals.inc)*100)
+			} else {
+				// percentage = -1 to show it is non-existent
+				data.percentage = -1; 
+				data.budget = 0;
+			}
+		},
+
+		getBudget: function() {
+			return {
+				budget: data.budget,
+				totalInc: data.totals.inc,
+				totalExp: data.totals.exp,
+				percentage: data.percentage,
+			}
 		},
 	}
 
@@ -156,11 +192,11 @@ let controller = (function(budgetCtrl, UICtrl) {
 	
 	let updateBudget = function() {
 		// 1. Calculate the budget
-		
+		budgetCtrl.calculateBudget();
 		// 2. Return the budget
-
+		let budget = budgetCtrl.getBudget();
 		// 3. Display the budget on the UI
-
+		console.log(budget);
 	};
 
 	let ctrlAddItem = function() {
